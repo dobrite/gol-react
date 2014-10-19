a83e00819e423448857f8f434a4b3aa4d52de041
Dave O <davidobrite@gmail.com>
2014-10-19 08:51:34 -0700

    1 gen

--- gol.js
+++ gol.js
@@ -1,53 +1,60 @@
 /** @jsx React.DOM */
 
-var Cells = React.createClass({
-  getInitialState: function() {
-    return {
-      cells: [
-        [0,0],
-        [1,1],
-    ]};
-  },
-
-  render: function() {
-    var rows = this.state.cells.map(function(row, i) {
-      var cells = this.state.cells[i].map(function(col, j) {
-        return (
-          <span
-            className={this.getClassName(i, j)}
-            key={'c' + i.toString() + j.toString()}
-            children='&nbsp;'>
-          </span>
-        );
-      }, this);
-
+var Gol = React.createClass({
+  render: function () {
+    var rows = this.props.array.map(function(row, i) {
       return (
-        <div
-          className='row'
-          key={'r' + i.toString()}>
-          {cells}
-        </div>
-      )
-    }, this);
+        <Row
+          key={'r' + i.toString()}
+          rowNumber={i}
+          row={row} />
+      );
+    });
 
     return (
       <div>
         {rows}
       </div>
-    )
+    );
+  }
+});
+
+var Row = React.createClass({
+  render: function () {
+    var cols = this.props.row.map(function(col, i) {
+      return (
+        <Col
+          key={'c' + i.toString()}
+          colNumber={i}
+          col={col} />
+      );
+    });
+
+    return (
+      <div className='row'>
+        {cols}
+      </div>
+    );
+  }
+});
+
+var Col = React.createClass({
+  render: function () {
+    return (
+      <span
+        className={this.getClassName()}
+        children='&nbsp;'/>
+    );
   },
 
-  getClassName: function (i, j) {
-    return (this.state.cells[i][j]) ? "live" : "dead";
+  getClassName: function () {
+    return (this.props.col) ? "live" : "dead";
   }
 });
 
 // https://gist.github.com/aemkei/1134658
-life = function(
-  input, // input array
-  size, // square stage size (width and height)
-  output, i, neighbours // placeholders
-){
+var life = function (input, output, i, neighbours){
+  var size = Math.sqrt(input.length);
   // cycle through cells
   for (
     output = [i = size*size];
@@ -66,9 +73,32 @@ life = function(
   }
 
   return output;
+};
+
+var initial = function () {
+  return [
+    0, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0, 0,
+    0, 1, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 0,
+    0, 0, 0, 1, 1, 0,
+    0, 0, 0, 0, 0, 0,
+  ];
+};
+
+var dtodd = function (arr) {
+  var l = arr.length,
+      d = Math.sqrt(l),
+      n = [];
+
+  for (i = 0; i < d; i++) {
+    n[i] = arr.slice(i * d, (i + 1) * d);
+  };
+
+  return n;
 }
 
 React.renderComponent(
-  <Cells />,
+  <Gol array={dtodd(life(initial()))} />,
   document.getElementById('gol')
 );
