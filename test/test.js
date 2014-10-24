40926a5a282dd11cf617fcea9d6e399ca8e4080c
Dave O <davidobrite@gmail.com>
2014-10-24 07:48:02 -0700

    save state

--- test/test.js
+++ test/test.js
@@ -18,31 +18,52 @@ var idx = function (loc, i, size, len) {
   };
 
   var bottom = function (val) {
-    console.log(val);
-    console.log(i);
-    console.log(len);
-    console.log(size);
-    console.log(val - i - len + size);
     return (val < len) ? val : (val - i) - len + size;
   };
 
   switch (loc) {
     case 0:
-      return top(i - size - 1);
+      if (i === loc) {
+        return len - 1;
+      } else {
+        return top(i - size - 1);
+      }
+      break;
     case 1:
       return top(i - size);
     case 2:
-      return top(i - size + 1);
+      if (i === loc) {
+        return len - size;
+      } else if ((i + 1) % size === 0) {
+        // right edge
+        return i - 2 * size + 1;
+      } else {
+        return top(i - size + 1);
+      }
+      break;
     case 3:
       return left(i - 1);
     case 5:
       return right(i + 1);
     case 6:
-      return bottom(i + size - 1);
+      if (i === loc) {
+        return size - 1;
+      } else if (i % size === 0) {
+        // left edge
+        return i + 2 * size - 1;
+      } else {
+        return bottom(i + size - 1);
+      }
+      break;
     case 7:
       return bottom(i + size);
     case 8:
-      return bottom(i + size + 1);
+      if (i === loc) {
+        return 0;
+      } else {
+        return bottom(i + size + 1);
+      }
+      break;
   }
 };
 
@@ -86,42 +107,84 @@ describe('Test 3x3 Array', function () {
   describe('i = 0', function () {
     describe('when loc is 0', function () {
       it('returns 8', function () {
-        assert.equal(8, idx(0, 0, 3, 9));
+        assert.equal(idx(0, 0, 3, 9), 8);
       });
     });
     describe('when loc is 1', function () {
       it('returns 6', function () {
-        assert.equal(6, idx(1, 0, 3, 9));
+        assert.equal(idx(1, 0, 3, 9), 6);
       });
     });
     describe('when loc is 2', function () {
       it('returns 7', function () {
-        assert.equal(7, idx(2, 0, 3, 9));
+        assert.equal(idx(2, 0, 3, 9), 7);
       });
     });
     describe('when loc is 3', function () {
       it('returns 2', function () {
-        assert.equal(2, idx(3, 0, 3, 9));
+        assert.equal(idx(3, 0, 3, 9), 2);
       });
     });
     describe('when loc is 5', function () {
       it('returns 1', function () {
-        assert.equal(1, idx(5, 0, 3, 9));
+        assert.equal(idx(5, 0, 3, 9), 1);
       });
     });
     describe('when loc is 6', function () {
-      it.only('returns 5', function () {
-        assert.equal(5, idx(6, 0, 3, 9));
+      it('returns 5', function () {
+        assert.equal(idx(6, 0, 3, 9), 5);
       });
     });
     describe('when loc is 7', function () {
       it('returns 3', function () {
-        assert.equal(3, idx(7, 0, 3, 9));
+        assert.equal(idx(7, 0, 3, 9), 3);
       });
     });
     describe('when loc is 8', function () {
       it('returns 4', function () {
-        assert.equal(4, idx(8, 0, 3, 9));
+        assert.equal(idx(8, 0, 3, 9), 4);
+      });
+    });
+  });
+  describe('i = 8', function () {
+    describe('when loc is 0', function () {
+      it('returns 4', function () {
+        assert.equal(idx(0, 8, 3, 9), 4);
+      });
+    });
+    describe('when loc is 1', function () {
+      it('returns 5', function () {
+        assert.equal(idx(1, 8, 3, 9), 5);
+      });
+    });
+    describe('when loc is 2', function () {
+      it('returns 3', function () {
+        assert.equal(idx(2, 8, 3, 9), 3);
+      });
+    });
+    describe('when loc is 3', function () {
+      it('returns 7', function () {
+        assert.equal(idx(3, 8, 3, 9), 7);
+      });
+    });
+    describe('when loc is 5', function () {
+      it('returns 6', function () {
+        assert.equal(idx(5, 8, 3, 9), 6);
+      });
+    });
+    describe('when loc is 6', function () {
+      it('returns 1', function () {
+        assert.equal(idx(6, 8, 3, 9), 1);
+      });
+    });
+    describe('when loc is 7', function () {
+      it('returns 2', function () {
+        assert.equal(idx(7, 8, 3, 9), 2);
+      });
+    });
+    describe('when loc is 8', function () {
+      it('returns 0', function () {
+        assert.equal(idx(8, 8, 3, 9), 0);
       });
     });
   });
